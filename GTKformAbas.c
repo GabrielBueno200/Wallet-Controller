bcc0b656108dd79889185cef33077b737aff5536
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2019-11-04 13:32:50 -0300

    abas login e cadastro - finalizadas
    GTK das abas que compõem o formulário de login e cadastro finalizado (basta upar o GTKformUsuario.c e .h finalizados para que essas abas funcionem)

--- GTKformAbas.c
+++ GTKformAbas.c
@@ -1,10 +1,13 @@
 #include <locale.h>
+#include <stdlib.h>
 #include <gtk/gtk.h>
 #include <string.h>
 #include "funcsCadastro.h"
 #include "funcsLogin.h"
+#include "funcsGTK.h"
 #define MAX 100
 
+//Reúne os dados do formulário de cadastro
 struct dadosCad{
     GtkWidget*  janela;
     GtkWidget* campoNome;
@@ -14,11 +17,20 @@ struct dadosCad{
     GtkTextBuffer *buffer;
 };
 
+//Reúne os dados do formulário de login
 struct dadosLogin{
     GtkWidget*  janela;
     GtkWidget*  campoCPF;
     GtkWidget*  campoSenha;
 };
+
+
+/*==============================================
+		VARIÁVEIS GLOBAIS
+===============================================*/
+struct dadosCad* cadFields;
+struct dadosLogin* loginFields;
+
 
 /*======================================================*/
 /*               ABA DE LOGIN DO USUÁRIO*/
@@ -27,6 +39,7 @@ struct dadosLogin{
 void adicionaAbaLogin(GtkWidget* windowForm, 
                       GtkWidget* formAbas, 
                       char* titulo, GtkWidget* ntAba){
+    
     //Variáveis para as labels
     GtkWidget *ntAbaLabel, *label_CPF, *label_Senha;
     
@@ -34,7 +47,11 @@ void adicionaAbaLogin(GtkWidget* windowForm,
     GtkWidget *campoCPF, *campoSenha, *btnLogin;
     
     //struct para armazenar os campos preenchidos durante o cadastro
-    struct dadosLogin *dados = malloc(sizeof(struct dadosLogin));
+    struct dadosLogin *loginFields = malloc(sizeof(struct dadosLogin));
+    
+    //===================================================================================
+                                       //ABA
+    //===================================================================================  
     
     //Cria uma label para exibir o título da aba
     ntAbaLabel = gtk_label_new (titulo);
@@ -43,44 +60,36 @@ void adicionaAbaLogin(GtkWidget* windowForm,
     //Cria uma aba para a página
     ntAba = gtk_layout_new (NULL, NULL);
     
-    //Label para o CPF do usuário
-    label_CPF = gtk_label_new ("Seu CPF:");
-    gtk_layout_put(GTK_LAYOUT(ntAba), label_CPF, 150, 95);
     
-    //Label para a senha do usuário
-    label_Senha = gtk_label_new ("Sua senha:");
-    gtk_layout_put(GTK_LAYOUT(ntAba), label_Senha, 150, 170);
     
-    //Campo para o CPF do usuário
-    campoCPF = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY(campoCPF), 11);
-    gtk_entry_set_width_chars(GTK_ENTRY(campoCPF), 22);
-    gtk_entry_set_placeholder_text (GTK_ENTRY(campoCPF), "Digite o seu CPF");
-    gtk_layout_put(GTK_LAYOUT(ntAba), campoCPF, 100, 120);
-    
-    //Campo para a senha do usuário
-    campoSenha = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY(campoSenha), 11);
-    gtk_entry_set_width_chars(GTK_ENTRY(campoSenha), 22);
+    //===================================================================================
+                                      //LABELS
+    //===================================================================================
+    criaLabel(&label_CPF, "Seu CPF:", "ignore", 150, 95, ntAba);
+    criaLabel(&label_Senha, "Sua senha:", "ignore", 150, 170, ntAba);
+    criaCampo(&campoCPF, 11, 22, "Digite o seu CPF", 100, 120, TRUE, FALSE, NULL, ntAba);
+    criaCampo(&campoSenha, 11, 22, "Digite a sua senha", 100, 195, TRUE, FALSE, NULL, ntAba);
     gtk_entry_set_visibility(GTK_ENTRY(campoSenha), FALSE);
-    gtk_entry_set_placeholder_text (GTK_ENTRY(campoSenha), "Digite a sua senha");
-    gtk_layout_put(GTK_LAYOUT(ntAba), campoSenha, 100, 195);
+
+    
+    //===================================================================================
+                              //BOTÕES TRADICIONAIS
+    //===================================================================================
+    criaBtn(&btnLogin, "Entrar",  145, 250, ntAba);
     
     
-    //Botão paa enviar os dados do formulário
-    btnLogin = gtk_button_new_with_label("Entrar");
-    gtk_layout_put(GTK_LAYOUT(ntAba), btnLogin, 145, 250);
-    
-    
+    //===================================================================================
+                  //EMPACOTAMENTO DOS WIDGETS E CALLBACKS + FUNÇÕES
+    //===================================================================================
     //Recolhe os dados de entrada e armazena no struct dados
-    dados->janela = windowForm;
-    dados->campoCPF = campoCPF;
-    dados->campoSenha = campoSenha;
+    loginFields->janela = windowForm;
+    loginFields->campoCPF = campoCPF;
+    loginFields->campoSenha = campoSenha;
 
     
     /*Quando o botão for clicado, iniciará o processo de login através da função validaLogin,
       que receberá como parâmetro os campos preenchidos do formulário*/
-    g_signal_connect (btnLogin, "clicked", G_CALLBACK (validaLogin), dados);
+    g_signal_connect (btnLogin, "clicked", G_CALLBACK (validaLogin), loginFields);
     
     
     
@@ -104,20 +113,21 @@ void adicionaAbaCadastro(GtkWidget* windowForm,
     //Variáveis para os campos
     GtkWidget *campoNome, *campoSaldo, *campoSenha, 
               *campoCPF, *btnCadastrar,
-              *scrolledwindow, *textarea;
+              *textarea;
     
     GtkTextBuffer *buffer;
-    GtkTextIter iter;
-    
-    //variável para incluir o arquivo de estilização do programa (estilo.css)
+
+    //importa o css
     GtkCssProvider *cssProvider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(cssProvider, "estilo.css", NULL);
-    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
-                               GTK_STYLE_PROVIDER(cssProvider),
-                               GTK_STYLE_PROVIDER_PRIORITY_USER);
+    importaCss(&cssProvider);
+    
     
     //struct para armazenar os campos preenchidos durante o cadastro
-    struct dadosCad *dados = malloc(sizeof(struct dadosCad));
+    struct dadosCad *cadFields = malloc(sizeof(struct dadosCad));
+    
+    //===================================================================================
+                                       //ABA
+    //===================================================================================  
     
     //Cria uma label para exibir o título da aba
     ntAbaLabel = gtk_label_new (titulo);
@@ -126,109 +136,58 @@ void adicionaAbaCadastro(GtkWidget* windowForm,
     //Cria uma aba para a página
     ntAba = gtk_layout_new (NULL, NULL);
     
-    //Label para o nome do usuário
-    label_Nome = gtk_label_new ("Seu Nome:");
-    gtk_layout_put(GTK_LAYOUT(ntAba), label_Nome, 70, 25);
+    
+    //===================================================================================
+                                      //LABELS
+    //===================================================================================
+    criaLabel(&label_Nome, "Seu Nome:", "ignore", 70, 25, ntAba);
+    criaLabel(&label_Categorias, "Suas Categorias\n(separadas por Enter):", "ignore", 200, 25, ntAba);
+    criaLabel(&label_Saldo, "Seu Saldo:", "ignore", 75, 95, ntAba);
+    criaLabel(&label_CPF, "Seu CPF:", "ignore", 80, 165, ntAba);
+    criaLabel(&label_Senha, "Sua senha:", "ignore", 80, 240, ntAba);
+    
+    
+    //===================================================================================
+                                      //CAMPOS
+    //===================================================================================
+    criaCampo(&campoNome, MAX, 22, "Digite o seu nome",30, 50, TRUE, FALSE, NULL, ntAba);
+    criaCampo(&campoSaldo, 11, 22, "Digite o seu saldo disponível",30, 120, TRUE, FALSE, NULL, ntAba);
+    criaCampo(&campoCPF, 11, 22, "Digite o seu CPF (apenas números)",30, 190, TRUE, FALSE, NULL, ntAba);
 
-    //Label para o nome do usuário
-    label_Categorias = gtk_label_new ("Suas Categorias:");
-    gtk_layout_put(GTK_LAYOUT(ntAba), label_Categorias, 200, 25);
-    
-    //Label para o saldo do usuário
-    label_Saldo = gtk_label_new ("Seu Saldo:");
-    gtk_layout_put(GTK_LAYOUT(ntAba), label_Saldo, 75, 95);
-    
-    //Label para o CPF do usuário
-    label_CPF = gtk_label_new ("Seu CPF:");
-    gtk_layout_put(GTK_LAYOUT(ntAba), label_CPF, 80, 165);
-    
-    //Label para a senha do usuário
-    label_Senha = gtk_label_new ("Sua senha:");
-    gtk_layout_put(GTK_LAYOUT(ntAba), label_Senha, 80, 240);
-    
-    
-    //Input para o nome do usuário
-    campoNome = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY(campoNome), 11);
-    gtk_entry_set_width_chars(GTK_ENTRY(campoNome), 22);
-    gtk_entry_set_placeholder_text (GTK_ENTRY(campoNome), "Digite o seu nome");
-    gtk_layout_put(GTK_LAYOUT(ntAba), campoNome, 30, 50);
-    
-    //Input para o saldo do usuário
-    campoSaldo = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY(campoSaldo), 11);
-    gtk_entry_set_width_chars(GTK_ENTRY(campoSaldo), 22);
-    gtk_entry_set_placeholder_text (GTK_ENTRY(campoSaldo), "Digite o seu saldo disponível");
-    gtk_layout_put(GTK_LAYOUT(ntAba), campoSaldo, 30, 120);
-    
-    //Input para o CPF do usuário
-    campoCPF = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY(campoCPF), 11);
-    gtk_entry_set_width_chars(GTK_ENTRY(campoCPF), 22);
-    gtk_entry_set_placeholder_text (GTK_ENTRY(campoCPF), "Digite o seu CPF (apenas números)");
-    gtk_layout_put(GTK_LAYOUT(ntAba), campoCPF, 30, 190);
-    
-    //Input para a senha do usuário
-    campoSenha = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY(campoSenha), 11);
-    gtk_entry_set_width_chars(GTK_ENTRY(campoSenha), 22);
+    criaCampo(&campoSenha, 11, 22, "Digite a sua senha",30, 265, TRUE, FALSE, NULL, ntAba);
     gtk_entry_set_visibility(GTK_ENTRY(campoSenha), FALSE);
-    gtk_entry_set_placeholder_text (GTK_ENTRY(campoSenha), "Digite a sua senha");
-    gtk_layout_put(GTK_LAYOUT(ntAba), campoSenha, 30, 265);
+
     
-    
+    //===================================================================================
+                                //TEXTAREA
+    //===================================================================================
     //TextArea para que o usuário digite as categorias desejadas
-    textarea = gtk_text_view_new();
-    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textarea));
+    criaTextArea(&textarea, &buffer, 150, 250, 200, 60,"textarea", TRUE, ntAba);
     
-    /*gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-    gtk_text_buffer_insert(buffer, &iter, "Categoria 1\n", -1);
-    gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-    gtk_text_buffer_insert(buffer, &iter, "Categoria 2\n", -1);
-    gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-    gtk_text_buffer_insert(buffer, &iter, "Categoria 3\n", -1);*/
-    
-    //Torna o TextArea editável
-    gtk_text_view_set_editable(GTK_TEXT_VIEW (textarea), TRUE);
-     
-    
-    
-    //Insere o TextArea em uma janela rolável
-    scrolledwindow = gtk_scrolled_window_new(NULL, NULL); 
-    
-    
-    //Adiciona o TextArea à janela rolável
-     gtk_container_add(GTK_CONTAINER(scrolledwindow), textarea);
-    
-    //Seta as dimensões da janela rolável
-     gtk_widget_set_size_request(scrolledwindow, 150, 250);
-    
-    //Adiciona uma classe à janela rolável
-    gtk_style_context_add_class(gtk_widget_get_style_context(scrolledwindow),"textarea");
-     
-    //Adiciona a janela rolável à janela principal
-    gtk_layout_put(GTK_LAYOUT(ntAba), scrolledwindow, 200, 50);
-     
-     
-   
+    //===================================================================================
+                              //BOTÕES TRADICIONAIS
+    //===================================================================================
     //Botão para submeter os dados obtidos no formulário de cadastro
     btnCadastrar= gtk_button_new_with_label("Cadastrar");
     gtk_layout_put(GTK_LAYOUT(ntAba), btnCadastrar, 125, 320);
    
     
-
+    //===================================================================================
+                  //EMPACOTAMENTO DOS WIDGETS E CALLBACKS + FUNÇÕES
+    //===================================================================================
+    
     //Recolhe os dados de entrada e armazena no struct dados
-    dados->janela = windowForm;
-    dados->campoNome = campoNome;
-    dados->campoCPF = campoCPF;
-    dados->campoSaldo = campoSaldo;
-    dados->campoSenha = campoSenha;
-    dados->buffer = buffer;
+    cadFields->janela = windowForm;
+    cadFields->campoNome = campoNome;
+    cadFields->campoCPF = campoCPF;
+    cadFields->campoSaldo = campoSaldo;
+    cadFields->campoSenha = campoSenha;
+    cadFields->buffer = buffer;
+    
     
     /*Quando o botão for clicado, iniciará o processo de cadastro através da função definePessoa,
       que receberá como parâmetro os campos preenchidos do formulário*/
-    g_signal_connect (btnCadastrar, "clicked", G_CALLBACK (definePessoa), dados);
-    
+    g_signal_connect (btnCadastrar, "clicked", G_CALLBACK (definePessoa), cadFields);
     
     
      //Adiciona à janela a aba criada
